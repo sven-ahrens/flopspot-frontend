535ba8991b15450d19d40c761a878a2e754412c5
Sven Ahrens <s.ahrens@djumla.de>
2020-04-25 18:09:17 +0200

    Add basic mock for train type results

--- src/views/TheTrain.vue
+++ src/views/TheTrain.vue
@@ -4,6 +4,20 @@
       {{ $t('pages.train.headline') }}
     </h1>
     <p>{{ $t('pages.train.text') }}</p>
+   <div class="trains">
+     <div
+       :class="{ 'trains__train--selected': train === pickedTrain }"
+       class="trains__train"
+       v-for="(train, key) in trains"
+       :key="key">
+       <input
+         :id="train"
+         type="radio"
+         :value="train"
+         v-model="pickedTrain">
+       <label :for="train">{{ train }}</label>
+     </div>
+   </div>
     <pagination next="/to" />
   </section>
 </template>
@@ -12,6 +26,12 @@
 import Pagination from '../components/Pagination'
 export default {
   name: 'TheTrain',
-  components: { Pagination }
+  components: { Pagination },
+  data () {
+    return {
+      trains: ['ICE 200', 'ICE 300', 'ICE 400'],
+      pickedTrain: ''
+    }
+  }
 }
 </script>
